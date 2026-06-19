1813d406b2acee499cde61f684734b02daddd65d
AmandaTi <151983009+Amandaad@users.noreply.github.com>
2026-06-19 14:08:53 -0300

    Add children page

---
+++ src/app/pages/children/children.page.ts
@@ -0,0 +1,3 @@
+import { Component } from '@angular/core';
+@Component({selector:'app-children',templateUrl:'children.page.html'})
+export class ChildrenPage { children=[{name:'Pedro',status:'Em dia'},{name:'Maria',status:'Pendente'}]; }
