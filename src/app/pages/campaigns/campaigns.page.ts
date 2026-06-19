4147f48306a05bfc3a4245a19f4a8bdef01d2b26
AmandaTi <151983009+Amandaad@users.noreply.github.com>
2026-06-19 14:08:33 -0300

    Add campaigns page

---
+++ src/app/pages/campaigns/campaigns.page.ts
@@ -0,0 +1,5 @@
+import { Component } from '@angular/core';
+@Component({selector:'app-campaigns',templateUrl:'campaigns.page.html'})
+export class CampaignsPage {
+ campaigns=[{title:'Influenza 2026',audience:'6 meses a 5 anos'}];
+}
